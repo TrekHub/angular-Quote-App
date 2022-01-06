1b653b4c1c8070e13a7d4f1da6bce6a854bb19f3
TrekHub <teddyomondi001@gmail.com>
2022-01-06 20:13:42 +0300

    create the quote component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    QuoteComponent,
   
   ],
   imports: [
