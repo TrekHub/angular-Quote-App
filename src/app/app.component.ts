55866c7220b8f1a6f09f961760437ee3e6703522
TrekHub <teddyomondi001@gmail.com>
2022-01-08 10:56:22 +0300

    add some styling to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,26 +39,7 @@ export class AppComponent {
       new Date('2022,01,09')
 
     ),
-    new Quote(
-      Quote.generateId(),
-      "Isaac newton",
-      "My name is Teddy Omondi, I love school",
-      "Teddy Omondi",
-      0,
-      0,
-      new Date('2022,01,09')
-
-    ),
-    new Quote(
-      Quote.generateId(),
-      "Isaac newton",
-      "My name is Teddy Omondi, I love school",
-      "Teddy Omondi",
-      0,
-      0,
-      new Date('2022,01,09')
-
-    )
+  
 
 
 
